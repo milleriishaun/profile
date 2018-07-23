fe35f418179f0d265cbd6d039a7bbac14bd1939c
milleriishaun <milleriishaun@gmail.com>
2018-07-23 10:38:30 -0700

    Edit sw-handler

--- sw-handler.js
+++ sw-handler.js
@@ -10,24 +10,25 @@ window.addEventListener('beforeinstallprompt', (e) => {
 
 let btnAdd = document.getElementById("addToHomescreen");
 
-//Check if the button on the phone is pressed
-btnAdd.addEventListener('click', (e) => {
-  // hide our user interface that shows our A2HS button
-  btnAdd.style.display = 'none';
-  // Show the prompt
-  deferredPrompt.prompt();
-  // Wait for the user to respond to the prompt
-  deferredPrompt.userChoice
-    .then((choiceResult) => {
-      if (choiceResult.outcome === 'accepted') {
-        console.log('User accepted the A2HS prompt');
-      } else {
-        console.log('User dismissed the A2HS prompt');
-      }
-      deferredPrompt = null;
+window.onload = function(){
+    //Check if the button on the phone is pressed
+    btnAdd.addEventListener('click', (e) => {
+    // hide our user interface that shows our A2HS button
+    btnAdd.style.display = 'none';
+    // Show the prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice
+        .then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+            console.log('User accepted the A2HS prompt');
+        } else {
+            console.log('User dismissed the A2HS prompt');
+        }
+        //deferredPrompt = null;
+        });
     });
-});
-
+}
 
 //determine if the app was successfully installed
 window.addEventListener('appinstalled', (evt) => {
