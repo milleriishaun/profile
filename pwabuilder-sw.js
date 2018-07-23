4b934a19d63a1098c3aeeef5e80ef81255a790c4
milleriishaun <milleriishaun@gmail.com>
2018-07-22 19:38:10 -0700

    Edit comment pwa-builder

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -19,7 +19,7 @@ var precacheFiles = [
   "images/9aeb90fa-c620-6833-50bf-5b3c51989a25.webPlatform.png",
   "images/581abbac-adec-559f-8558-d9b913810477.webPlatform.png",
   "images/939df4c4-1bad-3065-28e0-f1b8af7621cf.webPlatform.png",
-  "milleriishaun/images/4388dab3-2e5e-e5bb-0969-3afe1ef9e5cb.webPlatform.png",
+  "milleriishaun/images/4388dab3-2e5e-e5bb-0969-3afe1ef9e5cb.webPlatform.png", //this doesn't work because idk where to insert /milleriishaun/
   "images/b576374c-f3ef-1eb6-23a7-e83de53f4369.webPlatform.png",
   "images/c7e9a9c1-ef35-7887-3283-2f080d0c0c31.webPlatform.png",
   "images/e5fe5ba1-2e9f-25da-5a63-9645694c9bfc.webPlatform.png",
