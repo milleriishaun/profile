7204f87de5f53f4ff2ba3f55c96d8e0d98c6b5d3
milleriishaun <milleriishaun@gmail.com>
2018-07-23 09:31:45 -0700

    Edit manifest 2

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -72,40 +72,6 @@ var precacheFiles = [
   /* Add an array of files to precache for your app */
 ];
 
-let deferredPrompt;
-
-//notify user they can update
-window.addEventListener('beforeinstallprompt', (e) => {
-  e.preventDefault();
-  deferredPrompt = e;
-  // Update UI notify the user they can add to home screen
-  btnAdd.style.display = 'block';
-});
-
-//Check if the button on the phone is pressed
-btnAdd.addEventListener('click', (e) => {
-  // hide our user interface that shows our A2HS button
-  btnAdd.style.display = 'none';
-  // Show the prompt
-  deferredPrompt.prompt();
-  // Wait for the user to respond to the prompt
-  deferredPrompt.userChoice
-    .then((choiceResult) => {
-      if (choiceResult.outcome === 'accepted') {
-        console.log('User accepted the A2HS prompt');
-      } else {
-        console.log('User dismissed the A2HS prompt');
-      }
-      deferredPrompt = null;
-    });
-});
-
-
-//determine if the app was successfully installed
-window.addEventListener('appinstalled', (evt) => {
-  app.logEvent('a2hs', 'installed');
-});
-
 //Install stage sets up the cache-array to configure pre-cache content
 self.addEventListener('install', function (evt) {
   console.log('[PWA Builder] The service worker is being installed.');
