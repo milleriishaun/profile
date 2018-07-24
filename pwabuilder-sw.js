dae071e5c9013690635dd07805759f50d4ac169e
milleriishaun <milleriishaun@gmail.com>
2018-07-24 14:01:27 -0700

    Edit pro33

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -32,10 +32,10 @@ var CACHE = 'pwabuilder-precache';
 var precacheFiles = [
   "?homescreen=1",
 
-  "vendor/bootstrap/css/bootstrap.min.css",
-  "vendor/font-awesome/css/font-awesome.min.css",
-  "vendor/magnific-popup/magnific-popup.css",
-  "css/freelancer.min.css",
+  // "vendor/bootstrap/css/bootstrap.min.css",
+  // "vendor/font-awesome/css/font-awesome.min.css",
+  // "vendor/magnific-popup/magnific-popup.css",
+  // "css/freelancer.min.css",
 
   "images/2e6dc914-2f0a-62cb-aed2-04b951892d14.webPlatform.png",
   "images/3a295167-1a97-446e-d0e9-fb770a2c96f2.webPlatform.png",
@@ -95,10 +95,6 @@ var precacheFiles = [
   "img/mobile_icons/ms-icon-512x512_gradient_round.png",
   "img/favicon_32x32_gradient_round_07P_icon.ico",
   "img/favicon_32x32_gradient_round_07P_icon.ico",
-
-  'js/jqBootstrapValidation.js',
-  'js/contact_me.js',
-  'js/freelancer.min.js'
 
   // Add an array of files to precache for your app
 ];
--- sw-handler.js
+++ sw-handler.js
@@ -1,6 +1,6 @@
 
 let installPromptEvent;
-let btnInstall = document.querySelector("#addToHomescreen");
+let btnInstall = document.getElementById("addToHomescreen");
 
 window.addEventListener('beforeinstallprompt', (event) => {
   // Prevent Chrome <= 67 from automatically showing the prompt
@@ -8,12 +8,12 @@ window.addEventListener('beforeinstallprompt', (event) => {
   // Stash the event so it can be triggered later.
   installPromptEvent = event;
   // Update the install UI to notify the user app can be installed
-  document.querySelector('#addToHomescreen').disabled = false;
+  document.getElementById('addToHomescreen').disabled = false;
 });
 
 btnInstall.addEventListener('click', () => {
     // Update the install UI to remove the install button
-    document.querySelector('#addToHomescreen').disabled = true;
+    document.getElementById('addToHomescreen').disabled = true;
     // Show the modal add to home screen dialog
     installPromptEvent.prompt();
     // Wait for the user to respond to the prompt
