4d813da1f20218944228f68347338fbe26873fe9
milleriishaun <milleriishaun@gmail.com>
2018-07-27 18:09:06 -0700

    Edit pro55

--- sw-handler.js
+++ sw-handler.js
@@ -6,6 +6,7 @@ window.addEventListener('beforeinstallprompt', (event) => {
   // Prevent Chrome <= 67 from automatically showing the prompt
   event.preventDefault();
   // Stash the event so it can be triggered later.
+  console.log('tried to stash event');
   installPromptEvent = event;
   // Update the install UI to notify the user app can be installed
   document.getElementById('addToHomescreen').disabled = false;
@@ -14,6 +15,7 @@ window.addEventListener('beforeinstallprompt', (event) => {
 btnInstall.addEventListener('click', () => {
     // Update the install UI to remove the install button
     document.getElementById('addToHomescreen').disabled = true;
+    console.log('just failed to stash event');
     // Show the modal add to home screen dialog
     installPromptEvent.prompt();
     // Wait for the user to respond to the prompt
